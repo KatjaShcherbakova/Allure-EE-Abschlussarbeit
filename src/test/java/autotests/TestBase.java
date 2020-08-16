677a7562e6ed0cd854544dee251ed2457b6a4aa2
Katja <ekaterinas.scher@gmail.com>
2020-08-16 15:37:29 +0200

    TestBAse has been changed

--- src/test/java/autotests/TestBase.java
+++ src/test/java/autotests/TestBase.java
@@ -1,17 +1,15 @@
 package autotests;
 
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 
 import static autotests.helpers.DriverHelper.*;
 import static autotests.helpers.Environment.isVideoOn;
 import static helpers.AttachmentsHelper.*;
-import static helpers.AttachmentsHelper.attachVideo;
+
 
 public class TestBase {
 
@@ -19,7 +17,6 @@ public class TestBase {
     public static void setUp() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
         configureSelenide();
-        Configuration.startMaximized = true;
     }
 
     @AfterEach
