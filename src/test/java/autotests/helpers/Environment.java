df5e223eac5a4c053d4f81632d12b01fa2b68fc0
Katja <ekaterinas.scher@gmail.com>
2020-08-16 14:49:02 +0200

    variable names in Environment have been changed

--- src/test/java/autotests/helpers/Environment.java
+++ src/test/java/autotests/helpers/Environment.java
@@ -2,21 +2,21 @@ package autotests.helpers;
 
 public class Environment {
     public static final String
-            baseUrl = System.getProperty("BASE_URL","https://github.com"),
-            baseApiUrl = System.getProperty("BASE_URL","https://api.github.com"),
-            fileUrl = System.getProperty("FILE_URL","https://resources.github.com/whitepapers/How-GitHub-secures-open-source-software/"),
-            repository = System.getProperty("REPOSITORY","KatjaTest/Test-repository"),
-            tokenGithub = System.getProperty("TOKEN", ""),
-            usernameGithub = System.getProperty("USER_NAME","KatjaTest"),
-            passwordGithub = System.getProperty("PASSWORD_GITHUB", ""),
-            incorrectPasswordGithub = System.getProperty("INCORRECT_PASSWORD","fdfsfsfse"),
-            incorrectUsernameGithub = System.getProperty("INCORRECT_USERNAME","KatjTest"),
+            baseUrl = System.getProperty("base_url","https://github.com"),
+            baseApiUrl = System.getProperty("base_api_url","https://api.github.com"),
+            fileUrl = System.getProperty("page_with_file","https://resources.github.com/whitepapers/How-GitHub-secures-open-source-software/"),
+            repository = System.getProperty("repository","KatjaTest/Test-repository"),
+            tokenGithub = System.getProperty("token", ""),
+            usernameGithub = System.getProperty("user_name","KatjaTest"),
+            passwordGithub = System.getProperty("password", ""),
+            incorrectPasswordGithub = System.getProperty("incorrect_password","fdfsfsfse"),
+            incorrectUsernameGithub = System.getProperty("incorrect_password","KatjTest"),
 
 
-            remoteDriverUrl = System.getProperty("remote_driver_url"),
-            videoStorageUrl = System.getProperty("video_storage_url");
+            remoteDriverUrl = System.getProperty("remote_driver_url",""),
+            videoStorageUrl = System.getProperty("video_storage_url","");
 
      public static int
             correctIssueNumber = 1,
-            incorrectIssueNumber = 33333;
+            incorrectIssueNumber = 121457;
 }
